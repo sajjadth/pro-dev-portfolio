41b23311a2aa2e6d76d7f0fb0400229d7d1646a5
sajjad <st.655.st@gmail.com>
2023-11-29 12:20:17 +0330

    refactor: improve target element retrieval in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import "./styles/main.sass";
 import App from "./App.svelte";
 
+const targetElement = document.getElementById("app");
+
+if (!targetElement) {
+  throw new Error("Could not find the target element with id 'app'");
+}
+
 const app = new App({
-  target: document.getElementById("app"),
+  target: targetElement,
 });
 
 export default app;
