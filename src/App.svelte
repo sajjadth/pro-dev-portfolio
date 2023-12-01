8673f7409407801870dccee96a9ada2d86aedc7b
sajjad <st.655.st@gmail.com>
2023-12-01 14:09:55 +0330

    refactor: update content in App.svelte
    - Refactor the content in App.svelte to include a detailed introduction.
    - Enhance the structure with article tags for better semantic representation.
    - Improve styling and layout.

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,25 @@
 <script lang="ts">
 </script>
 
-<main
-  id="container"
-  class="w-100 h-100 d-flex align-items-center justify-content-center flex-column"
->
-  <h1 id="title-name">Sajjad<br />Tahmouresi</h1>
+<main class="w-100 h-100 container d-flex align-items-center justify-content-center flex-column">
+  <article>
+    <h1 id="title-name">I'm <span id="title-name-colored">Sajjad Tahmouresi</span></h1>
+
+    <p>
+      As a full-stack developer based in Iran, I dedicate my days to transforming lines of code into
+      immersive web experiences. Specializing in JavaScript and Golang, I can seamlessly work with
+      any new technology, embracing a versatile approach to bring ideas to life.
+    </p>
+    <p>
+      My journey in technology is rooted in self-teaching, evolving from a passionate enthusiast to
+      a proficient coder. I thrive on continuous learning, unafraid to explore new technologies and
+      always open to trying new things.
+    </p>
+    <p>
+      Beyond the screen, I find inspiration in simplicity and order, applying a minimalistic touch
+      to my work. My commitment to creating elegant solutions extends to my love for
+      problem-solving. Let's connect and embark on a journey to craft exceptional digital
+      experiences together.
+    </p>
+  </article>
 </main>
