d92275ad82e735630b4efcaf69e7798d2dd9dda4
sajjad <st.655.st@gmail.com>
2023-12-02 13:08:14 +0330

    feat: add Header component
    - Introduce a new file 'src/components/Header.svelte' containing header navigation.
    - Enhance 'src/App.svelte' to include the newly added 'Header' component.
    - Improve navigation with a header that includes links to 'About,' 'Projects,' and 'Contact.'

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import Router from "svelte-spa-router";
   import Home from "./routes/Home.svelte";
+  import Header from "./components/Header.svelte";
 
   const routes = {
     "/": Home,
   };
 </script>
 
+<Header />
 <Router {routes} />
---
+++ src/components/Header.svelte
@@ -0,0 +1,12 @@
+<header class="container">
+  <nav class="w-100 d-flex flex-row align-items-center justify-content-start">
+    <a href="/#" class="not-link">
+      <div id="navbar-icon" />
+    </a>
+    <ul class="w-100 d-flex flex-row align-items-center justify-content-end">
+      <li><a href="#/about">About</a></li>
+      <li><a href="#/projects">Projects</a></li>
+      <li><a href="#/contact">Contact</a></li>
+    </ul>
+  </nav>
+</header>
