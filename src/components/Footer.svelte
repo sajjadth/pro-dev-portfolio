4e1476ced987bf31a5c62e62abe7b7cc1f7ad891
sajjad <st.655.st@gmail.com>
2023-12-03 19:52:20 +0330

    feat: add Footer component
    - Introduce a new 'Footer.svelte' component containing social media links (GitHub, LinkedIn, Twitter, Twitch, Buy Me a Coffee).
    - Enhance the project's user engagement and connectivity with various social platforms.

---
+++ src/components/Footer.svelte
@@ -0,0 +1,22 @@
+<footer class="container d-flex flex-row align-items-center justify-content-start">
+  <a class="not-link social-link" id="github" href="https://github.com/sajjadth" target="_blank">
+  </a>
+  <a
+    class="not-link social-link"
+    id="linkedin"
+    href="https://linkedin.com/in/sajjadth"
+    target="_blank"
+  >
+  </a>
+  <a class="not-link social-link" id="twitter" href="https://twitter.com/sajjaadth" target="_blank">
+  </a>
+  <a class="not-link social-link" id="twitch" href="https://twitch.com/SonTheWitch" target="_blank">
+  </a>
+  <a
+    class="not-link social-link"
+    id="bmc"
+    href="https://www.buymeacoffee.com/sajjadth"
+    target="_blank"
+  >
+  </a>
+</footer>
