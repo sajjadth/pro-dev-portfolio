9026dcdb7e5eca90279e19abdb08488c1f8de9fe
sajjad <st.655.st@gmail.com>
2023-12-03 11:27:27 +0330

    style: improve responsiveness and styling
    - Set minimum width of the body to 100% for better responsiveness.
    - Update link styles, adding overflow for '.link' class to handle potential overflow.
    - Adjust the 'min-width' property in the body to ensure better adaptability to various screen sizes.
    - Refine background gradients, padding, and layout in the '#app' section for improved visual aesthetics.
    - Enhance media queries to optimize layout at different breakpoints, ensuring a seamless user experience.

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,9 +4,9 @@
       <div id="navbar-icon" />
     </a>
     <ul class="w-100 d-flex flex-row align-items-center justify-content-end">
-      <li><a href="#/about">About</a></li>
-      <li><a href="#/projects">Projects</a></li>
-      <li><a href="#/contact">Contact</a></li>
+      <li class="link"><a href="#/about">About</a></li>
+      <li class="link"><a href="#/projects">Projects</a></li>
+      <li class="link"><a href="#/contact">Contact</a></li>
     </ul>
   </nav>
 </header>
