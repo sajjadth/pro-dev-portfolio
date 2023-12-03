5d6639538164d00c5bd73fcd1c721dfe793e9674
sajjad <st.655.st@gmail.com>
2023-12-03 11:57:25 +0330

    style: highlight active link in Header component
    - Update 'src/components/Header.svelte' to dynamically highlight the active link based on the current path.
    - Introduce styles in 'src/styles/main.sass' to enhance the visual representation of the active link.

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,12 +1,25 @@
+<script lang="ts">
+  let currentPath = window.location.hash.slice(2, window.location.hash.length).toLowerCase();
+  const updatePath = () =>
+    (currentPath = window.location.hash.slice(2, window.location.hash.length)).toLowerCase();
+  window.addEventListener("hashchange", updatePath);
+</script>
+
 <header class="container">
   <nav class="w-100 d-flex flex-row align-items-center justify-content-start">
     <a href="/#" class="not-link">
       <div id="navbar-icon" />
     </a>
     <ul class="w-100 d-flex flex-row align-items-center justify-content-end">
-      <li class="link"><a href="#/about">About</a></li>
-      <li class="link"><a href="#/projects">Projects</a></li>
-      <li class="link"><a href="#/contact">Contact</a></li>
+      <li class="link">
+        <a href="#/about" id={currentPath === "about" ? "active-link" : ""}>About</a>
+      </li>
+      <li class="link">
+        <a href="#/projects" id={currentPath === "projects" ? "active-link" : ""}>Projects</a>
+      </li>
+      <li class="link">
+        <a href="#/contact" id={currentPath === "contact" ? "active-link" : ""}>Contact</a>
+      </li>
     </ul>
   </nav>
 </header>
