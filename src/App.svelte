c4091111095e5e690ebd4b402bfb2ab0855b8277
sajjad <st.655.st@gmail.com>
2023-12-05 19:55:22 +0330

    feat: add About page to routes
    Include the About page in the list of routes, enabling navigation to the new About Me section. This update enhances the overall structure of the application, allowing users to explore additional content beyond the home page.

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,11 @@
   import Home from "./routes/Home.svelte";
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
+  import About from "./routes/About.svelte";
 
   const routes = {
     "/": Home,
+    "/about": About,
   };
 </script>
 
