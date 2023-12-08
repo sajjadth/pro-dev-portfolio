8e2e6a0f4d697ba70f501182c5e8ad435eb1026d
sajjad <st.655.st@gmail.com>
2023-12-08 15:46:28 +0330

    feat: implement loading state and completion feedback in Contact form
    Introduce a loading state and completion feedback to the Contact form. Disable form elements during the loading state and provide visual feedback upon completion. Utilize the Svelte fade transition for a smoother user experience.

--- src/routes/Contact.svelte
+++ src/routes/Contact.svelte
@@ -1,19 +1,37 @@
 <script lang="ts">
   import { fade } from "svelte/transition";
 
+  let loading = false;
+  let done = false;
   let email = "";
   let name = "";
   let message = "";
+
   function sendMessage() {
-    console.log("name:", name);
-    console.log("email:", email);
-    console.log("message:", message);
+    loading = true;
+    fetch(
+      `https://pro-dev-portfolio.sajjadth.workers.dev/?name=${name}&email=${email}&message=${message}`,
+      {
+        method: "GET",
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          done = true;
+          console.log("done");
+        } else {
+          console.log("not done");
+        }
+      })
+      .catch((err) => console.log("Error:", err))
+      .finally(() => (loading = false));
   }
 </script>
 
 <main
-in:fade={{delay:500}}
-out:fade|local
+  in:fade={{ delay: 500 }}
+  out:fade|local
   class="w-100 h-100 container d-flex align-items-center justify-content-start flex-column"
 >
   <article>
@@ -23,21 +41,60 @@ out:fade|local
       <a href="mailto:sajjaadth@pm.me" class="link"> sajjaadth@pm.me </a>
     </p>
     <form
-      on:submit|preventDefault|once={sendMessage}
+      on:submit|preventDefault={sendMessage}
       class="d-flex flex-column align-items-start justifu-content center"
     >
-      <input type="text" bind:value={name} name="name" placeholder="Name" required />
-      <input type="email" bind:value={email} name="email" placeholder="Email" required />
+      <input
+        disabled={loading || done}
+        type="text"
+        bind:value={name}
+        name="name"
+        placeholder="Name"
+        required
+      />
+      <input
+        disabled={loading || done}
+        type="email"
+        bind:value={email}
+        name="email"
+        placeholder="Email"
+        required
+      />
       <textarea
+        disabled={loading || done}
         bind:value={message}
         name="message"
         placeholder="Message"
         cols="1"
         rows="10"
-        maxlength="128"
+        maxlength="512"
         required
       />
-      <button type="submit">Send Message</button>
+      <button disabled={loading || done} type="submit">
+        {#if done}
+          Done
+        {:else if loading}
+          <svg
+            class="spinner"
+            width="65px"
+            height="65px"
+            viewBox="0 0 66 66"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle
+              class="path"
+              fill="none"
+              stroke-width="6"
+              stroke-linecap="round"
+              cx="33"
+              cy="33"
+              r="30"
+            ></circle>
+          </svg>
+        {:else}
+          Send Message
+        {/if}
+      </button>
     </form>
   </article>
   <h1 class="subtle-background-title subtle-background-title-lg">Contact.</h1>
@@ -50,6 +107,46 @@ out:fade|local
   p {
     text-wrap: balance;
   }
+  $offset: 187;
+  $duration: 1.4s;
+
+  .spinner {
+    animation: rotator $duration linear infinite;
+    width: 20px;
+    height: 20px;
+  }
+
+  @keyframes rotator {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(270deg);
+    }
+  }
+
+  .path {
+    stroke-dasharray: $offset;
+    stroke-dashoffset: 0;
+    transform-origin: center;
+    stroke: #000;
+    animation: dash $duration ease-in-out infinite;
+  }
+
+  @keyframes dash {
+    0% {
+      stroke-dashoffset: $offset;
+    }
+    50% {
+      stroke-dashoffset: calc($offset / 4);
+      transform: rotate(135deg);
+    }
+    100% {
+      stroke-dashoffset: $offset;
+      transform: rotate(450deg);
+    }
+  }
+
   main {
     article {
       text-align: justify;
@@ -80,6 +177,7 @@ out:fade|local
           width: 100%;
         }
         button {
+          width: 160px;
           background: colors.$primary-color;
           border: none;
           color: colors.$background-color;
@@ -87,13 +185,22 @@ out:fade|local
           padding: 10px 20px;
           margin-block: 8px;
           transition: all 250ms ease;
+          position: relative;
           &:hover {
-            background-color: #a2d837;
-            transition: all 250ms ease;
+            &:not(:disabled) {
+              background-color: #a2d837;
+              transition: all 250ms ease;
+            }
           }
           &:active {
-            transform: scale(0.95);
-            transition: all 250ms ease;
+            &:not(:disabled) {
+              transform: scale(0.95);
+              transition: all 250ms ease;
+            }
+          }
+          &:disabled {
+            cursor: not-allowed;
+            background: #d3d3d3;
           }
         }
         input,
@@ -106,12 +213,16 @@ out:fade|local
           border-radius: 8px;
           transition: all 250ms ease;
           &:hover {
-            border-color: colors.$text-color;
+            &:not(:disabled) {
+              border-color: colors.$text-color;
+            }
           }
           &:focus {
-            outline: none;
-            border-color: colors.$text-color;
-            background: #0000009a;
+            &:not(:disabled) {
+              outline: none;
+              border-color: colors.$text-color;
+              background: #0000009a;
+            }
           }
         }
         textarea {
