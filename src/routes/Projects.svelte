883cc19d51978553b5e4db4e889bd1ab574733e3
sajjad <st.655.st@gmail.com>
2023-12-12 20:40:47 +0330

    feat: implement Projects page layout and styling
    Introduce the Projects page with dynamic content rendering for project cards. Utilize Svelte fade transitions and horizontal chip scrolling. Add external link icons for GitHub and project sites. Adjust layout and styling for improved responsiveness.

--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -2,6 +2,17 @@
   import { onMount } from "svelte";
   import { fade } from "svelte/transition";
 
+  const projects = [
+    {
+      title: "Persian Calendar",
+      description:
+        "Persian Calendar, the simple and stylish widget that will help you keep track of your days. With this widget, you can see a beautiful and intuitive calendar in your notion pages.",
+      technologies: ["JavaScript", "React", "Redux", "Sass"],
+      github: "https://github.com/sajjadth/persian-calendar",
+      site: "https://sajjadth.github.io/persian-calendar",
+    },
+  ];
+
   onMount(() => {
     const scrollContainer = document.getElementById("horizontal-chip");
 
@@ -19,7 +30,32 @@
 >
   <article>
     <h1 id="title">Projects.</h1>
-    <div class="d-flex flex-row align-items-center-justify-centent-start w-100 h-100"></div>
+    <div id="projects" class="h-100">
+      {#each projects as { title, description, technologies, github, site }}
+        <div class="card">
+          <div class="card-title">
+            <h2 class="text-break">{title}</h2>
+          </div>
+          <div class="card-text">
+            <p>{description}</p>
+          </div>
+          <div
+            id="horizontal-chip"
+            class="card-interactions d-flex align-items-center justify-content-start"
+          >
+            {#each technologies as name}
+              <div class={`chip ${name.toLowerCase()}`}>{name}</div>
+            {/each}
+          </div>
+          <div
+            class="card-external-links w-100 d-flex flex-row align-items-center justify-content-end"
+          >
+            <a class="github not-link" href={github} target="_blank" />
+            <a class="external-link not-link" href={site} target="_blank" />
+          </div>
+        </div>
+      {/each}
+    </div>
   </article>
   <h1 class="subtle-background-title subtle-background-title-lg">Projects.</h1>
 </main>
@@ -28,49 +64,11 @@
   @use "src/styles/_variables.sass";
   @use "src/styles/_colors.sass";
 
-  .external-links {
-    width: fit-content;
-    height: 20px;
-    position: absolute;
-    top: 24px;
-    right: 24px;
-    .external-link {
-      background: url("src/assets/images/external-link.svg") no-repeat center;
-      background-size: cover;
-      width: 20px;
-      height: 20px;
-      transition: all 250ms ease;
-      cursor: pointer;
-      border-radius: 100%;
-      margin-left: 4px;
-      &:hover {
-        transform: scale(1.1);
-      }
-      &:active {
-        transform: scale(0.9);
-      }
-    }
-    .github {
-      background: url("src/assets/images/github.svg") no-repeat center;
-      background-size: cover;
-      width: 20px;
-      height: 20px;
-      transition: all 250ms ease;
-      cursor: pointer;
-      border-radius: 100%;
-      &:hover {
-        transform: scale(1.1);
-      }
-      &:active {
-        transform: scale(0.9);
-      }
-    }
-  }
-
   main {
     article {
       text-align: justify;
       width: 725px;
+      min-height: fit-content;
       @media (max-width: variables.$breakpoint-lg) {
         width: 100%;
       }
@@ -90,73 +88,135 @@
           font-size: 3rem;
         }
       }
-      .card {
-        width: 300px;
-        min-height: 300px;
-        background: red;
-        border-radius: 20px;
-        background: rgba(0, 0, 0, 0.3);
-        box-shadow: 0 0 4px 0 rgb(78, 81, 92, 0.37);
-        backdrop-filter: blur(5px);
-        border: 1px solid rgba(255, 255, 255, 0.18);
-        transition: all 250ms ease;
-        padding: 8px 24px;
-        &:hover {
+      #projects {
+        width: fit-content;
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap: 16px;
+        justify-content: center;
+        align-items: center;
+        justify-items: center;
+        .card {
+          width: 300px;
+          min-height: 300px;
+          background: red;
+          border-radius: 20px;
+          background: rgba(0, 0, 0, 0.3);
+          box-shadow: 0 0 4px 0 rgb(78, 81, 92, 0.37);
+          backdrop-filter: blur(5px);
+          border: 1px solid rgba(255, 255, 255, 0.18);
           transition: all 250ms ease;
-          backdrop-filter: blur(7.5px);
-          box-shadow: 0 0 16px 0 rgb(78, 81, 92, 0.37);
-        }
-        &-title {
-          width: 100%;
-          height: 40%;
-          border-radius: 12px;
-          padding-block: 16px;
-          display: flex;
-          align-items: flex-start;
-          justify-content: center;
-          text-align: left !important;
-        }
-        &-text {
-          width: 100%;
-          height: 45%;
-          // background: darkslateblue;
-          padding-block: 8px;
-          border-radius: 12px;
-          text-align: left;
-
-          p {
-            height: 100%;
+          padding: 8px 24px;
+          &:hover {
+            transition: all 250ms ease;
+            backdrop-filter: blur(7.5px);
+            box-shadow: 0 0 16px 0 rgb(78, 81, 92, 0.37);
+          }
+          &-title {
             width: 100%;
-            margin: 0;
-
-            display: -webkit-box;
-            -webkit-line-clamp: 5;
-            -webkit-box-orient: vertical;
-            overflow: hidden;
-          }
-        }
-        &-interactions {
-          width: 100%;
-          height: 15%;
-          padding-block: 8px;
-          margin-bottom: 8px;
-          overflow-y: hidden;
-          overflow-x: scroll;
-          &::-webkit-scrollbar {
-            display: none !important;
-          }
-          .chip {
-            width: fit-content;
-            height: fit-content;
-            border-radius: 20px;
-            padding: 2px 8px;
-            font-size: 14px;
+            height: 40%;
+            border-radius: 12px;
+            padding-block: 16px 8px;
             display: flex;
-            align-items: center;
+            align-items: flex-start;
             justify-content: center;
-            margin-right: 8px;
-            &:last-child {
-              margin-right: 0;
+            text-align: left !important;
+            margin: 0;
+          }
+          &-text {
+            width: 100%;
+            height: 45%;
+            // background: darkslateblue;
+            padding-block: 8px;
+            border-radius: 12px;
+            text-align: left;
+
+            p {
+              height: 100%;
+              width: 100%;
+              margin: 0;
+
+              display: -webkit-box;
+              -webkit-line-clamp: 5;
+              -webkit-box-orient: vertical;
+              overflow: hidden;
+            }
+          }
+          &-interactions {
+            width: 100%;
+            height: 15%;
+            padding-block: 8px;
+            margin-bottom: 8px;
+            overflow-y: hidden;
+            overflow-x: scroll;
+            &::-webkit-scrollbar {
+              display: none !important;
+            }
+            .chip {
+              width: fit-content;
+              height: fit-content;
+              border-radius: 20px;
+              padding: 2px 8px;
+              font-size: 14px;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              margin-right: 8px;
+              &:last-child {
+                margin-right: 0;
+              }
+            }
+            .react {
+              background: rgb(0, 216, 255, 0.25);
+            }
+            .sass {
+              background: rgb(204, 102, 153, 0.25);
+            }
+            .redux {
+              background: rgb(118, 74, 188, 0.25);
+            }
+            .javascript {
+              background: rgb(240, 219, 79, 0.25);
+            }
+            .nodejs {
+              background: rgb(60, 135, 58, 0.25);
+            }
+          }
+          &-external-links {
+            width: fit-content;
+            height: 20px;
+            margin-block: 8px;
+
+            .external-link {
+              background: url("src/assets/images/external-link.svg") no-repeat center;
+              background-size: cover;
+              width: 20px;
+              height: 20px;
+              transition: all 250ms ease;
+              cursor: pointer;
+              border-radius: 100%;
+              margin-left: 8px;
+              &:hover {
+                transform: scale(1.1);
+              }
+              &:active {
+                transform: scale(0.9);
+              }
+            }
+            .github {
+              background: url("src/assets/images/github.svg") no-repeat center;
+              background-size: cover;
+              width: 20px;
+              height: 20px;
+              transition: all 250ms ease;
+              cursor: pointer;
+              border-radius: 100%;
+              &:hover {
+                transform: scale(1.1);
+              }
+              &:active {
+                transform: scale(0.9);
+              }
             }
           }
         }
